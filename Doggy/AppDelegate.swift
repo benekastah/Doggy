56892dac1251042528ef1ad9f1c2faa5fc6270c5
Paul Harper <paul@chartio.com>
2017-02-02 15:39:35 -0800

    More better things

--- Doggy/AppDelegate.swift
+++ Doggy/AppDelegate.swift
@@ -16,14 +16,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
     var darkModeOn = false
     
-    var commandPressed = false;
-    var controlPressed = false;
-    var shiftPressed = false;
     var leftPressed = false;
     var rightPressed = false;
-    var downPressed = false;
     var upPressed = false;
-
+    var downPressed = false;
+    var shiftPressed = false;
+    var timer: Timer?;
+    
+    let xPosIncrement = 4.0 / 13.0;
+    let xNormalSize = 5.0 / 13.0;
+    let xLargeSize = 9.0 / 13.0;
+    
+    let yPosIncrement = 6.0 / 13.0;
+    let ySize = 7.0 / 13.0;
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -42,57 +47,68 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         self.statusItem.menu = menu
         
-        if (!AXIsProcessTrusted()) {
-            let alert = NSAlert()
-            alert.messageText = "Doggy doesn't have accessibility enabled. Keyboard shortcuts won't work without it. Go to System Preferences > Security & Privacy > Privacy > Accessibility to enable it."
-            let img = Bundle.main.pathForImageResource("Doggy-128")
-            alert.icon = NSImage.init(byReferencingFile: img!)
-            alert.runModal()
-        }
-        
-        NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.keyDown.union(NSEventMask.flagsChanged),
-                                          handler: {(event) in self.handleGlobalEvents(event: event, pressed: true)})
-        NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.keyUp.union(NSEventMask.flagsChanged),
-                                          handler: {(event) in self.handleGlobalEvents(event: event, pressed: false)})
+        let options : NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+        AXIsProcessTrustedWithOptions(options)
+        setUpGlobalEvents()
     }
     
-    func handleGlobalEvents(event: NSEvent, pressed: Bool) {
-        commandPressed = event.modifierFlags.contains(NSEventModifierFlags.command)
-        controlPressed = event.modifierFlags.contains(NSEventModifierFlags.control)
-        shiftPressed = event.modifierFlags.contains(NSEventModifierFlags.shift)
-        if event.keyCode == 123 {
-            leftPressed = pressed;
-        } else if event.keyCode == 124 {
-            rightPressed = pressed;
-        } else if event.keyCode == 125 {
-            downPressed = pressed;
-        } else if event.keyCode == 126 {
-            upPressed = pressed;
+    func setUpGlobalEvents() -> Void {
+        if AXIsProcessTrusted() {
+            NSEvent.addGlobalMonitorForEvents(matching: NSEventMask.keyUp.union(NSEventMask.flagsChanged),
+                                              handler: handleGlobalEvents)
+        } else {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(_: Timer) in self.setUpGlobalEvents()})
         }
-        
-        if commandPressed && controlPressed {
+    }
+    
+    func handleGlobalEvents(event: NSEvent) {
+        let command = event.modifierFlags.contains(NSEventModifierFlags.command)
+        let control = event.modifierFlags.contains(NSEventModifierFlags.control)
+        if command && control {
+            shiftPressed = shiftPressed || event.modifierFlags.contains(NSEventModifierFlags.shift)
+            upPressed = upPressed || event.keyCode == 126
+            rightPressed = rightPressed || event.keyCode == 124
+            downPressed = downPressed || event.keyCode == 125
+            leftPressed = leftPressed || event.keyCode == 123
+            timer?.invalidate()
+            timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false, block: delayedHandleGlobalEvents)
+        }
+    }
+    
+    func delayedHandleGlobalEvents(t: Timer) {
+        if shiftPressed {
             if leftPressed {
-                if shiftPressed {
-                    pullLeftBig(sender: self)
-                } else {
-                    pullLeft(sender: self)
-                }
-                leftPressed = false;
-            } else if downPressed {
-                pullMiddle(sender: self)
-                downPressed = false
+                pullLeftBig(sender: self)
             } else if rightPressed {
-                if shiftPressed {
-                    pullRightBig(sender: self)
-                } else {
-                    pullRight(sender: self)
-                }
-                rightPressed = false
+                pullRightBig(sender: self)
             } else if upPressed {
-                fullScreen(sender: self)
-                upPressed = false
+                pullMiddleTop(sender: self)
+            } else if downPressed {
+                pullMiddleBottom(sender: self)
             }
+        } else if leftPressed && downPressed {
+            pullLeftBottom(sender: self)
+        } else if leftPressed && upPressed {
+            pullLeftTop(sender: self)
+        } else if leftPressed {
+            pullLeft(sender: self)
+        } else if rightPressed && downPressed {
+            pullRightBottom(sender: self)
+        } else if rightPressed && upPressed {
+            pullRightTop(sender: self)
+        } else if rightPressed {
+            pullRight(sender: self)
+        } else if upPressed {
+            fullScreen(sender: self)
+        } else if downPressed {
+            pullMiddle(sender: self)
         }
+    
+        shiftPressed = false
+        leftPressed = false
+        rightPressed = false
+        downPressed = false
+        upPressed = false
     }
     
     func quit(sender: AnyObject) {
@@ -103,36 +119,61 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
-    func positionWindows(segment: Int, size: Int, numSegments: Int) {
+    func positionWindows(x: Double, y: Double, width: Double, height: Double) {
         let task = Process()
         task.launchPath = "/usr/bin/osascript"
         let script = Bundle.main.path(forResource: "position-window", ofType: "scpt")!
-        task.arguments = [script, String(segment), String(size), String(numSegments)]
+        let args = [x, y, width, height].map({String($0)});
+        task.arguments = [script] + args;
         task.launch()
     }
     
     func pullLeft(sender: AnyObject) {
-        positionWindows(segment: 0, size: 1, numSegments: 3)
+        positionWindows(x: 0, y: 0, width: self.xNormalSize, height: 1)
+    }
+    
+    func pullLeftTop(sender: AnyObject) {
+        positionWindows(x: 0, y: 0, width: self.xNormalSize, height: self.ySize)
+    }
+    
+    func pullLeftBottom(sender: AnyObject) {
+        positionWindows(x: 0, y: self.yPosIncrement, width: self.xNormalSize, height: self.ySize)
     }
     
     func pullMiddle(sender: AnyObject) {
-        positionWindows(segment: 1, size: 1, numSegments: 3)
+        positionWindows(x: self.xPosIncrement, y: 0, width: self.xNormalSize, height: 1)
+    }
+    
+    func pullMiddleTop(sender: AnyObject) {
+        positionWindows(x: self.xPosIncrement, y: 0, width: self.xNormalSize, height: self.ySize)
+    }
+    
+    func pullMiddleBottom(sender: AnyObject) {
+        positionWindows(x: self.xPosIncrement, y: self.yPosIncrement, width: self.xNormalSize, height: self.ySize)
     }
     
     func pullRight(sender: AnyObject) {
-        positionWindows(segment: 2, size: 1, numSegments: 3)
+        positionWindows(x: 2 * self.xPosIncrement, y: 0, width: self.xNormalSize, height: 1)
+    }
+    
+    func pullRightTop(sender: AnyObject) {
+        positionWindows(x: 2 * self.xPosIncrement, y: 0, width: self.xNormalSize, height: self.ySize)
+    }
+    
+    func pullRightBottom(sender: AnyObject) {
+        positionWindows(x: 2 * self.xPosIncrement, y: self.yPosIncrement, width: self.xNormalSize, height: self.ySize)
     }
     
     func pullLeftBig(sender: AnyObject) {
-        positionWindows(segment: 0, size: 2, numSegments: 3)
+        positionWindows(x: 0, y: 0, width: self.xLargeSize, height: 1)
     }
     
     func pullRightBig(sender: AnyObject) {
-        positionWindows(segment: 1, size: 2, numSegments: 3)
+        positionWindows(x: self.xPosIncrement, y: 0, width: self.xLargeSize, height: 1)
     }
     
     func fullScreen(sender: AnyObject) {
-        positionWindows(segment: 0, size: 1, numSegments: 1)
+        positionWindows(x: 0, y: 0, width: 1, height: 1)
     }
 }
 
--- Doggy/position-window.applescript
+++ Doggy/position-window.applescript
@@ -1,18 +1,20 @@
 #!/usr/bin/osascript -e
 
 on run argv
-    set selectedSegment to item 1 of argv
-    set selectedSize to item 2 of argv
-    set numSegments to item 3 of argv
+    set x to item 1 of argv
+    set y to item 2 of argv
+    set width to item 3 of argv
+    set height to item 4 of argv
+    
     tell application "Finder"
         set _b to bounds of window of desktop
         set _w to item 3 of _b
         set _h to item 4 of _b
     end tell
+    
     tell application "System Events"
         set proc to first process where it is frontmost
-        set segmentW to _w * (1 / numSegments)
-        set position of front window of proc to {segmentW * selectedSegment, 0}
-        set size of front window of proc to {segmentW * selectedSize, _h}
+        set position of front window of proc to {x * _w, y * _h}
+        set size of front window of proc to {width * _w, height * _h}
     end tell
 end run
